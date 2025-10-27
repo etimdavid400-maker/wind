07c95fe3fc3c43b7684855aa2f8ada76c9208729
etim david <etimdavid400@gmail.com>
2025-10-27 10:48:41 -0700

    Fix contact import path (case sensitivity)

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Navbar from './component/Navbar';
 import About from './pages/About';
-import Contact from './pages/contact';
+import Contact from './pages/Contact';
 import Footer from './component/Footer';
 import Blog from './pages/Blog';
 import Service from './pages/Services';
