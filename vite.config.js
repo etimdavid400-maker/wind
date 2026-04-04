90082fc5c8bb88ee45f7f1c176963a23701a2dd5
etim david <etimdavid400@gmail.com>
2026-04-04 00:47:47 -0800

    updated something sha

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import sitemap from 'vite-plugin-sitemap'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig({
   plugins: [
     react(),
     sitemap({
-      hostname: 'https://www.otan.org.ng'
-    })
-  ]
-})
+      hostname: 'https://www.otan.org.ng',
+    }),
+  ],
+  base: '/', // ✅ Ensure your JS/CSS are loaded correctly on production
+});
