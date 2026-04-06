9ae1bdf6f1b41d26b12c7a7165188b055e21e6ef
etim david <etimdavid400@gmail.com>
2026-04-05 17:09:13 -0800

    fix: added tailwindcss vite plugin, styles now working

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 // vite.config.js
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react';
 import sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     react(),
     sitemap({
       hostname: 'https://www.otan.org.ng',
